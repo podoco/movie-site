10583c87d87e57bd9b9c2b2b24485564849d3216
podoco <103816228+podoco@users.noreply.github.com>
2022-08-28 23:47:25 +0900

    home,api,routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
+import Movie from "./components/Movie";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [movies, setMovies] = useState([])
+
+  const getMovies = async () => {
+    const response = await fetch(
+      `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+    );
+    const json = await response.json();
+    setMovies(json.data.movies);
+    setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <div>
+          {movies.map((movie) => (
+            <Movie
+              key={movie.id}
+              coverImg={movie.medium_cover_image}
+              title={movie.title}
+              summary={movie.summary}
+              genres={movie.genres}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
+
+
+
+
+
+
+
+
+
+// function App() {
+
+//   const [loading, setLoading] = useState(true);
+//   const [coins, setCoins] = useState([]);
+
+//   useEffect(() => {
+//     fetch("https://api.coinpaprika.com/v1/tickers")
+//       .then((response) => response.json())
+//       .then((json) => {
+//         setCoins(json);
+//         setLoading(false);
+//       });
+//   }, []);
+
+//   return (
+//     <div>
+//       <h1>The Coins!{loading ? "" :  `(${coins.length})`} </h1>
+//       {loading ? <strong>Loading...</strong> : <select>
+//         {coins.map((coin) => (
+//           <option>
+//             {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
+//           </option>
+//         ))}
+//       </select>
+//       }
+//     </div>
+//   );
+// }
 
 export default App;
